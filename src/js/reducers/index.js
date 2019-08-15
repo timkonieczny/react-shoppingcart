ba5a613a0cd656ca14c6952d9e84b8402c8380ea
Tim Konieczny <timkonieczny@gmail.com>
2019-08-15 12:23:23 +1200

    added comments

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -15,10 +15,9 @@ const rootReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_SHOPPING_CART_ITEM:
             {
-                const index = state.shoppingCartItems.map(item => item.name).indexOf(action.payload.name)
-                let newItem = action.payload
+                const index = state.shoppingCartItems.map(item => item.name).indexOf(action.payload.name) // see if the item is already in the cart
                 if (index !== -1) {
-                    const quantity = state.shoppingCartItems[index].quantity + 1
+                    const quantity = state.shoppingCartItems[index].quantity + 1 // it is, increase the quantity
                     return Object.assign({}, state, {
                         shoppingCartItems: state.shoppingCartItems.map((item, i) => {
                             if (i === index)
@@ -30,6 +29,7 @@ const rootReducer = (state = initialState, action) => {
                         })
                     })
                 } else {
+                    let newItem = action.payload // it's not, add it to cart
                     newItem.quantity = 1
                     return Object.assign({}, state, {
                         shoppingCartItems: state.shoppingCartItems.concat(newItem)
@@ -38,16 +38,16 @@ const rootReducer = (state = initialState, action) => {
             }
         case REMOVE_SHOPPING_CART_ITEM:
             {
-                const index = state.shoppingCartItems.map(item => item.name).indexOf(action.payload.name)
-                if (state.shoppingCartItems[index].quantity === 1)
+                const index = state.shoppingCartItems.map(item => item.name).indexOf(action.payload.name) // see how many of this item are in cart
+                if (state.shoppingCartItems[index].quantity === 1) // 1, remove the item
                     return Object.assign({}, state, {
-                        shoppingCartItems: [
-                            ...state.shoppingCartItems.slice(0, index),
-                            ...state.shoppingCartItems.slice(index + 1)
-                        ]
-                    })
+                    shoppingCartItems: [
+                        ...state.shoppingCartItems.slice(0, index),
+                        ...state.shoppingCartItems.slice(index + 1)
+                    ]
+                })
                 else {
-                    const quantity = action.payload.quantity - 1
+                    const quantity = action.payload.quantity - 1 // more than 1, reduce the quantity
                     return Object.assign({}, state, {
                         shoppingCartItems: state.shoppingCartItems.map((item, i) => {
                             if (i === index)
